644e74f1678a68132a1075a019cb213fd77476b8
Leon Vinicius <lyon.vns@gmail.com>
2026-01-05 17:35:37 -0300

    Finalizando dia 15

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,32 +1,72 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from data import MENU, resources
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+lucro = 0
+MAQUINA_LIGADA = True
 
+
+def verificar_recursos(ingredientes_bebida):
+    """Retorna True se houver recursos suficientes, False se não"""
+    for i in ingredientes_bebida:
+
+        if ingredientes_bebida[i] > resources.get(i, 0):
+            print(f"Sorry there is not enough {i}.")
+            return False
+    return True
+
+
+def processar_moedas():
+    """Retorna o valor total das moedas inseridas[cite: 23]."""
+    print("Please insert coins.")
+
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.10
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def transacao_bem_sucedida(dinheiro_recebido, custo_bebida):
+    """Verifica se o dinheiro é suficiente e calcula troco."""
+    if dinheiro_recebido >= custo_bebida:
+
+        troco = round(dinheiro_recebido - custo_bebida, 2)
+        if troco > 0:
+            print(f"Here is ${troco} in change.")
+
+        global lucro
+        lucro += custo_bebida
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+
+def fazer_cafe(nome_bebida, ingredientes_bebida):
+    """Deduz os ingredientes e serve a bebida"""
+    for i in ingredientes_bebida:
+        resources[i] -= ingredientes_bebida[i]
+    print(f"Here is your {nome_bebida} ☕. Enjoy!")
+
+
+while MAQUINA_LIGADA:
+
+    escolha = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if escolha == "off":
+        MAQUINA_LIGADA = False
+        print("Machine shutting down...")
+
+    elif escolha == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${lucro}")
+
+    elif escolha in MENU:
+        bebida = MENU[escolha]
+
+        if verificar_recursos(bebida["ingredients"]):
+            pagamento = processar_moedas()
+            if transacao_bem_sucedida(pagamento, bebida["cost"]):
+
+                fazer_cafe(escolha, bebida["ingredients"])
