87d7abbceb268b906a5497fe28673a8ab85efea7
Leon Vinicius <lyon.vns@gmail.com>
2026-01-13 00:56:12 -0300

    Finalizando dia 18

---
+++ Day 18/Different_Shapes.py
@@ -0,0 +1,29 @@
+import random
+from turtle import Turtle, Screen
+
+tim = Turtle()
+
+tim.shape("turtle")
+
+colours = ["red", "blue", "green", "yellow", "orange", "purple", "cyan", "magenta", "lime", "gold", "turquoise", "dodgerblue"]
+
+
+num_sides = 5
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3,11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
---
+++ Day 18/Random_Walk.py
@@ -0,0 +1,24 @@
+import random
+from turtle import Turtle, Screen
+
+tim = Turtle()
+directions = [0, 90, 180, 270]
+
+screen = Screen()
+screen.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+screen.exitonclick()
