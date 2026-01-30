81a24d8ecee0507623e85fa51417f9cfe3c06580
Leon Vinicius <lyon.vns@gmail.com>
2026-01-29 23:22:30 -0300

    Snake Game with wrap around

---
+++ Day 21/main.py
@@ -0,0 +1,51 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Score
+import time
+
+
+screen = Screen()
+screen.setup(width= 600, height= 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = Score()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+
+game_in_on = True
+
+while game_in_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.reflesh()
+        snake.extend()
+        score.increase_score()
+
+    snake.wrap_around()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+
+        elif snake.head.distance(segment) < 10:
+            game_in_on = False
+            score.game_over()
+
+
+
+screen.exitonclick()
